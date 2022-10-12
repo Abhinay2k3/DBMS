066d0fa05e4942589554cb3b012daf54644a57b0
balivada abhinay <94745716+Abhinay2k3@users.noreply.github.com>
2022-10-12 15:47:27 +0530

    Update univ.sql

--- univ.sql
+++ univ.sql
@@ -15,3 +15,20 @@ insert into course values('9549','btech','4587')
 insert into course values('9550','btech','4588')
 insert into course values('9551','btech','4589')
 insert into course values('9552','btech','4585')
+select *from course
+create table faculty (fac_id int not null,name varchar(10))
+insert into faculty values(2020,'gautham')
+insert into faculty values(null,'anil')
+select *from faculty
+create table subject(sub_id int unique,credits int)
+insert into subject values('93478','4')
+insert into subject values('93479','4')
+insert into subject values('93480','4')
+insert into subject values('93481','4')
+insert into subject values(null,'8')
+select *from subject
+create table worker(name varchar(50),age int check (age>=18))
+insert into worker values('shamitha',19)
+insert into worker values('mani',17)
+insert into worker values('niharika',20)
+select *from worker
