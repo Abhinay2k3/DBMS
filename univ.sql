017e83cf4386d362d8e36e853827c4f75b2a16ab
balivada abhinay <94745716+Abhinay2k3@users.noreply.github.com>
2022-10-12 15:23:54 +0530

    Update univ.sql

--- univ.sql
+++ univ.sql
@@ -7,3 +7,11 @@ select *from student
 insert into student values('4587','lehari','female','btech')
 insert into student values('4588','shamitha','female','btech')
 insert into student values('4589','pallavi','female','btech')
+create table course(course_id int not null primary key,name varchar(20) not null, id int,foreign key (id) references student(id));
+select *from course
+insert into course values('9547','btech','4585')
+insert into course values('9548','btech','4586')
+insert into course values('9549','btech','4587')
+insert into course values('9550','btech','4588')
+insert into course values('9551','btech','4589')
+insert into course values('9552','btech','4585')
