188f13145615c47dba4596151e781f00331839e7
balivada abhinay <94745716+Abhinay2k3@users.noreply.github.com>
2022-10-12 15:03:03 +0530

    Create univ.sql

---
+++ univ.sql
@@ -0,0 +1,9 @@
+create database univ
+use univ
+create table student(Id int not null primary key,name varchar(50) not null,gender varchar(30),course varchar(45));
+insert into student values('4585','chaitanya','male','btech')
+insert into student values('4586','ajay','male','btech')
+select *from student
+insert into student values('4587','lehari','female','btech')
+insert into student values('4588','shamitha','female','btech')
+insert into student values('4589','pallavi','female','btech')
