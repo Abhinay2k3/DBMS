fb422f57c8550e21ec9ec69c6f7529593932fb40
balivada abhinay <94745716+Abhinay2k3@users.noreply.github.com>
2022-11-02 15:52:52 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -20,36 +20,37 @@ insert into job values(669,'analyst');
 insert into job values(670,'salesperson');
 insert into job values(671,'manager');
 insert into job values(672,'president');
-create table employee(employee_id int,last_name varchar(100),first_name varchar(100),middle_name varchar(100),job_id int, manager_id varchar(100), hiredate varchar(29), salary int,comm int, department_id int);
-select *from employee;
-insert into employee values(7369,'smith','john','q',667,7902,'17-dec-84',800,null,20);
-insert into employee values(7499,'allen','kevin','j',670,7698,'20-feb-85',1600,300,30);
-insert into employee values(7505,'doyle','jean','k',661,7839,'4-apr-85',2850,null,30);
-insert into employee values(7506,'dennis','lynn','s',671,7839,'15-may-85',2750,null,30);
-insert into employee values(7507,'baker','leslie','d',671,7839,'10-jun-85',4400,null,40);
-insert into employee values(7521,'wark','cynthia','d',670,7698,'22-feb-85',1250,500,30);
-select *from employee;
+create table employe(employe_id int,last_name varchar(100),first_name varchar(100),middle_name varchar(100),job_id int, manager_id varchar(100), hiredate varchar(29), salary int,comm int, department_id int);
+select *from employe;
+insert into employe values(7369,'smith','john','q',667,7902,'17-10-84',800,null,20);
+insert into employe values(7499,'allen','kevin','j',670,7698,'20-02-85',1600,300,30);
+insert into employe values(7505,'doyle','jean','k',661,7839,'4-04-85',2850,null,30);
+insert into employe values(7506,'dennis','lynn','s',671,7839,'15-05-85',2750,null,30);
+insert into employe values(7507,'baker','leslie','d',671,7839,'10-06-85',4400,null,40);
+insert into employe values(7521,'wark','cynthia','d',670,7698,'22-02-85',1250,500,30);
+select *from employe;
 select *from DEPARTMENT;
 select *from job;
 select *from location;
-select *from employee where last_name='smith'
-select *from employee;
-select first_name,last_name,salary,comm from employee
-select employee_id"id of the employee",last_name"name of the employee",department_id"Department ID" from employee
-select last_name, salary*12 "annual salary" from employee
-select *from employee where last_name='SMITH';
-select *from employee where department_id=20
-select *from employee where salary between 3000 and 4500
-select *from employee where department_id in (20,30)
-select last_name,salary,comm,department_id from employee where department_id not in (10,30)
-select *from employee where last_name like 'S%'
-select *from employee where last_name like 'S%H'
-select *from employee where last_name like 'S___'
-select *from employee where department_id=10 and salary>3500
-select *from employee where comm is NULL
-select employee_id,last_name from employee order by employee_id asc
-select employee_id,last_name from employee order by salary desc
-select *from employee order by last_name,salary desc
-select *from employee order by last_name,department_id desc
-select count(distinct department_id) from employee
-select avg(salary), max(salary),min(salary) from employee order by department_id
+select *from employe where last_name='smith'
+select *from employe;
+select first_name,last_name,salary,comm from employe
+select employe_id"id of the employe",last_name"name of the employe",department_id"Department ID" from employe
+select last_name, salary*12 "annual salary" from employe
+select *from employe where last_name='SMITH';
+select *from employe where department_id=20
+select *from employe where salary between 3000 and 4500
+select *from employe where department_id in (20,30)
+select last_name,salary,comm,department_id from employe where department_id not in (10,30)
+select *from employe where last_name like 'S%'
+select *from employe where last_name like 'S%H'
+select *from employe where last_name like 'S___'
+select *from employe where department_id=10 and salary>3500
+select *from employe where comm is NULL
+select employe_id,last_name from employe order by employe_id asc
+select employe_id,last_name from employe order by salary desc
+select *from employe order by last_name,salary desc
+select *from employe order by last_name,department_id desc
+select count(distinct department_id) from employe
+select avg(salary), max(salary),min(salary) from employe order by department_id
+select *from employe group by month(hiredate) order by hiredate
